410c1422ddbf1912ed65c9d57aa6e431cbaf83f6
Wasimkhan1997 <124414568+Wasimkhan1997@users.noreply.github.com>
2023-04-10 20:44:16 +0530

    Add files via upload

---
+++ assignment6apr.js
@@ -0,0 +1,48 @@
+// Question 1
+let vari1 = 2;
+console.log(vari1);
+
+//Function AddNumber
+addNumber();
+let a = 10;
+let b = 20;
+ 
+function addNumber (){
+    
+    console.log(a + b);
+}
+
+//Question 2
+let m = 12;
+let j = 13;
+multiplyNumber();
+function multiplyNumber(){
+    console.log(m*j)
+}
+
+//Question 3
+addNumber();
+ 
+function addNumber (){
+    var one = 4;
+    var two = 6;
+    console.log(one + two);
+}
+
+//Question 4
+
+block();
+function block(){
+    var point1 = 40;
+    let point2 = 30;
+    const point3 = 40;
+    console.log(point1 ,point2, point3)
+}
+
+//Question 5 
+js();
+function js(){
+    let skills = 
+    console.log(skills)
+}
+
